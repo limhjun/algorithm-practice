d00762fa588fc106edf0c6b465bb4e3221a52277
hyungjunn <hotfirebulldog@gmail.com>
2026-03-04 13:53:43 +0900

    백준 8958: OX 퀴즈 리팩토링

--- baekjoon/8958.cpp
+++ baekjoon/8958.cpp
@@ -2,8 +2,6 @@
 
 using namespace std;
 
-int ox_cnt[82];
-
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
@@ -12,28 +10,18 @@ int main() {
   cin >> N;
 
   string ox;
-  for (int i= 0; i < N; i++) {
-    // 길이가 0보다 크고 80보다 작은 문자열
-    int sum = 0;
+  for (int i = 0; i < N; i++) {
     cin >> ox;
-    if (ox[0] == 'O') {
-      ox_cnt[0] = 1;
-      sum++;
-    } else {
-      ox_cnt[0] = 0;
-    }
-    for (int j = 1 ; j < ox.length(); j++) {
-      if (ox[j] == 'O' && ox_cnt[j - 1] != 0) {
-        ox_cnt[j] = ox_cnt[j - 1] + 1;
-        sum += ox_cnt[j];
-      } else if (ox[j] == 'O' && ox_cnt[j - 1] == 0) {
-        ox_cnt[j] = 1;
-        sum += ox_cnt[j];
+
+    int sum = 0, streak = 0;
+    for (char c : ox) {
+      if (c == 'O') {
+        streak++;
+        sum += streak;
       } else {
-        ox_cnt[j] = 0;
+        streak = 0;
       }
     }
-
     cout << sum << "\n";
   }
 
