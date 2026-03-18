fac456b9bcb6568167f1391226be27ec529eb073
hyungjunn <hotfirebulldog@gmail.com>
2026-03-18 10:50:16 +0900

    백준 2751 수 정렬하기 2: 완료(MergeSort)

--- baekjoon/2751.cpp
+++ baekjoon/2751.cpp
@@ -2,6 +2,45 @@
 
 using namespace std;
 
+void MergeSort(int* begin, int* end) {
+  if (end - begin <= 1) return;
+
+  int* mid = begin + ((end - begin) / 2);
+  MergeSort(begin, mid);
+  MergeSort(mid, end);
+  int arr[end - begin];
+  int* i = begin;
+  int* j = mid;
+  int cur = 0;
+  while (i != mid && j != end) {
+    if (*i <= *j) {
+      arr[cur] = *i;
+      i++;
+    } else {
+      arr[cur] = *j;
+      j++;
+    }
+    cur++;
+  }
+  if (i == mid) {
+    while (j != end) {
+      arr[cur++] = *j;
+      j++;
+    }
+  } else if (j == end) {
+    while (i != mid) {
+      arr[cur++] = *i;
+      i++;
+    }
+  }
+
+  int k = 0;
+  while (begin != end) {
+    *begin = arr[k++];
+    begin++;
+  }
+}
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
@@ -16,7 +55,8 @@ int main() {
     v.push_back(num);
   }
 
-  sort(v.begin(), v.end());
+  // sort(v.begin(), v.end());
+  MergeSort(&v[0], &v[v.size()]);
 
   for (int i = 0; i < v.size(); i++) cout << v[i] << "\n";
 
